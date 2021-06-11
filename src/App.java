0861ca8abeb82d68b075f4e6e3db1d83dc2c0960
Mythyndian <zawistowskimaciej1@gmail.com>
2021-06-11 16:10:07 +0200

    File names are depended on url

--- src/App.java
+++ src/App.java
@@ -10,6 +10,8 @@ import java.net.URL;
 public class App {
     public static void main(String[] args) throws Exception {
         findFiles("png", "https://github.com/");
+        findLinks("https://github.com/");
+
     }
 
     public static boolean isUrl(String url) {
@@ -27,23 +29,37 @@ public class App {
 
     // Method for finding files with certain extension
     public static void findFiles(String extension, String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.getElementsByAttribute("href");
-        for (Element link : links) {
-            String linkHref = link.attr("href");
-            //if (linkHref.endsWith("." + extension)) {
-                // Save to file with a file name as url of page were it's found
-                String filename = "page" + "_" + extension + "s";
-                writeResults(filename, linkHref);
-            //}
-        }
+        String clearUrl;
+        int counter = 0;
+        if (isUrl(url)) {
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.getElementsByAttribute("href");
+            for (Element link : links) {
+                String linkHref = link.attr("href");
+                if (linkHref.endsWith("." + extension)) {
+                    // TODO link find method
+                    if (url.contains("https://")) {
+                        clearUrl = url.replace("/", "");
+                        clearUrl = clearUrl.replace("https:", "");
+                    } else {
+                        clearUrl = url.replace("/", "");
+                        clearUrl = clearUrl.replace("http:", "");
+                    }
+
+                    String filename = clearUrl + "_" + extension + "s";
+                    writeResults(filename, linkHref);
+                }
+            }
+
+        } else
+            System.out.println("Incorect URL.");
     }
 
     public static void writeResults(String filename, String data) {
         String currentDirectory = System.getProperty("user.dir");
         String lineSeparator = System.getProperty("line.separator");
         try {
-            File dump = new File( currentDirectory + File.separator + "collectedData" + File.separator + filename + ".txt");
+            File dump = new File(currentDirectory + File.separator + "collectedData" + File.separator + filename + ".txt");
             if (dump.createNewFile()) {
                 System.out.println("Creating file: " + dump.getName() + "...");
                 FileWriter fw = new FileWriter(dump);
@@ -55,8 +71,6 @@ public class App {
                 fw.write(data + lineSeparator);
                 fw.flush();
                 fw.close();
-                System.out.println("Entry successfully added!");
-
             }
         } catch (IOException e) {
             System.out.println("An error occurred.");
@@ -64,4 +78,25 @@ public class App {
         }
     }
 
+    public static void findLinks(String url) throws IOException {
+        String clearUrl;
+        String linkHref;
+        if (isUrl(url)) {
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.getElementsByTag("a");
+            for (Element link : links) {
+                linkHref = link.attr("href");
+                if (url.contains("https://")) {
+                    clearUrl = url.replace("/", "");
+                    clearUrl = clearUrl.replace("https:", "");
+                } else {
+                    clearUrl = url.replace("/", "");
+                    clearUrl = clearUrl.replace("http:", "");
+                }
+
+                String filename = clearUrl + "_" + "links";
+                writeResults(filename, linkHref);
+            }
+        }
+    }
 }
